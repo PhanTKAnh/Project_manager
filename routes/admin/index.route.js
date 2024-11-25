24143272ee001e6386b6967f25d78bfd34fc6f00
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2024-11-25 17:24:52 +0700

    Dang nhap

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productRoutes = require("./product.route")
 const productCategoryRoutes = require("./product-category.route")
 const rolesRoute = require("./roles.route")
 const accountRoute = require("./account.route")
+const authRoute = require("./ath.route")
 module.exports = (app) => {
   const PATCH_ADMIN =sytemConfig.prefixAdmin;
    app.use(PATCH_ADMIN + "/dashboard",dashboardRoutes);
@@ -11,5 +12,6 @@ module.exports = (app) => {
    app.use(PATCH_ADMIN + "/product-category",productCategoryRoutes)
    app.use(PATCH_ADMIN + "/roles",rolesRoute)
    app.use(PATCH_ADMIN + "/account",accountRoute)
+   app.use(PATCH_ADMIN + "/auth",authRoute)
   
 }
