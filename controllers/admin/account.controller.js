2a28fa3546108ae507bb0beebd0957ae4638adc6
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2024-11-25 16:25:56 +0700

    lam trang sua tai khoan

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -50,3 +50,53 @@ module.exports.createPost = async (req, res) => {
     }
 
 }
+//[GET] /admin/account/edit/:id
+module.exports.edit = async (req, res) => {
+    const find ={
+        _id:req.params.id,
+        deleted:false
+    };
+    try {
+        const data = await Account.findOne(find);
+       
+            const role = await Role.find({
+                deleted:false
+            });
+          
+    
+        res.render("admin/pages/account/edit", {
+            pageTitle: "Chỉnh sửa  khoản mới ",
+            data:data,
+            role:role
+    
+        });
+        
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/account`);
+    }
+    
+}
+//[PATCH] /admin/account/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    const emailExist = await Account.findOne({
+        _id:{$ne :id},
+        email:req.body.email,
+        deleted:false
+    });
+    if(emailExist){
+        req.flash("error",`email ${req.body.email} đã tồn tại `);
+    }else{
+        if(req.body.password){
+            req.body.password=md5(req.body.password);
+        }else{
+            delete req.body.password;
+        }
+    
+        await Account.updateOne({_id:id},req.body);
+        req.flash("success","Cập nhật thành công ")
+    }
+   
+    res.redirect("back");
+    
+}
--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -19,3 +19,18 @@ module.exports.createPost= (req,res,next) =>{
     next();
  
 }
+module.exports.editPatch= (req,res,next) =>{
+    if(!req.body.fullname){
+        req.flash("error","Vui lòng nhập tên họ  ");
+        res.redirect("back");
+        return;
+    }
+
+    if(!req.body.email){
+        req.flash("error","Vui lòng nhập email");
+        res.redirect("back");
+        return;
+    }
+    next();
+ 
+}
