cc2baf696af792388ee7c8b4473506cf2313f59e
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2024-12-01 18:00:08 +0700

    tao collection vs model order

---
+++ models/order.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater')
+mongoose.plugin(slug);
+const orderSchema = new mongoose.Schema(
+    {
+        // user_id:String,
+        cart_id:String,
+        userInfo:{
+            fullName:String,
+            phone:String,
+            address:String
+        },
+        products:[
+            {
+                product_id:String,
+                price:Number,
+                quantity:String,
+                discountPercentage:Number
+        }
+        ],
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deleteAt: Date
+
+    }, {
+    timestamps: true
+}
+);
+const Order = mongoose.model('Order', roleSchema, "order");
+
+module.exports = Order;
